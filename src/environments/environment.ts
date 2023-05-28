ed8a821227a95b43ed52ca15abb4fc71f77b7391
Dan Chui <dchui108@gmail.com>
2023-05-28 21:29:57 +0900

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAR50a3zxIdppk0yj1Kf8tULlSENlsCSqU',
+    authDomain: 'clips-b90c6.firebaseapp.com',
+    projectId: 'clips-b90c6',
+    storageBucket: 'clips-b90c6.appspot.com',
+    appId: '1:556113969255:web:088f321be137545401d4c2',
+  },
+};
